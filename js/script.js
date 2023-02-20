373dd0be613f53f72d3e6a5e9ee04dbad3357da6
Tanya <example@gmail.com>
2023-02-20 16:37:53 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt ("Вам есть 18?", "18")
+//console.log (answer);
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?' , '');
+//answers[1] = prompt('Как ваша фамилия?' , '');
+//answers[2] = prompt('Сколько вам лет?' , '');
+
+//document.write(answers);
+
+//const user = "Ivan";
+
+//alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log (incr);
+console.log (decr);
---
+++ script1.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = true;
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
